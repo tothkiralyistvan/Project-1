70c6890eb5a28ed0d228c035b551dfffb6441190
tothkiralyistvan <40205277+tothkiralyistvan@users.noreply.github.com>
2023-07-19 17:16:01 -0400

    Update R code.R

--- R code.R
+++ R code.R
@@ -1,3 +1,12 @@
-## Placeholder for the R code associated with this project.
-## To be replaced later on with updated code.
-## xxxss.
+# Placeholder for the R code associated with this project.
+# To be replaced later on with updated code.
+
+# Libraries
+library(haven)
+library(tidyverse)
+library(janitor)
+library(sjmisc)
+library(sjstats)
+library(psych)
+library(summarytools)
+library(skimr)
